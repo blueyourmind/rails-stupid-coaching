68ca8933df87ffee14e3cf045ac721fe4f0bf542
Thierry <raymondmclou@gmail.com>
2023-09-26 21:16:07 +0400

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,2 @@
-class QuestionsController < ApplicationController
-  def personal_coach(text)
-    if text.start_with?("I am going to work right now!")
-      "That's good to hear! Now go out there and make it happen!"
-    else
-      "Get dressed and go to work!"
-    end
-  end
-
-  def answer
-    @question = params[:question]
-    @answer = personal_coach(@question)
-    render :answer
-  end
-
-  def ask
-    render :ask
-  end
+class QuestionsController < ActionController::Base
 end
